49668cb0fe7bac087aad66b3fcd1fcb7342c6926
Xiaoliang Wu <zurielibra@gmail.com>
2016-02-13 03:05:38 -0600

    initial project

---
+++ buffer_mgr.c
@@ -0,0 +1,284 @@
+#include "buffer_mgr.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "dberror.h"
+#include "storage_mgr.h"
+
+/*
+ // Replacement Strategies
+typedef enum ReplacementStrategy {
+  RS_FIFO = 0,
+  RS_LRU = 1,
+  RS_CLOCK = 2,
+  RS_LFU = 3,
+  RS_LRU_K = 4
+} ReplacementStrategy;
+
+// Data Types and Structures
+typedef int PageNumber;
+#define NO_PAGE -1
+
+typedef struct BM_BufferPool {
+  char *pageFile;
+  int numPages;
+  ReplacementStrategy strategy;
+  void *mgmtData; // use this one to store the bookkeeping info your buffer 
+                  // manager needs for a buffer pool
+} BM_BufferPool;
+
+typedef struct BM_PageHandle {
+  PageNumber pageNum;
+  char *data;
+} BM_PageHandle;
+
+ */
+
+
+// Buffer Manager Interface Pool Handling
+
+/***************************************************************
+ * Function Name: 
+ * 
+ * Description:
+ *
+ * Parameters:
+ *
+ * Return:
+ *
+ * Author:
+ *
+ * History:
+ *      Date            Name                        Content
+ *
+***************************************************************/
+
+RC initBufferPool(BM_BufferPool *const bm, const char *const pageFileName, 
+		  const int numPages, ReplacementStrategy strategy, 
+		  void *stratData){
+}
+
+/***************************************************************
+ * Function Name: 
+ * 
+ * Description:
+ *
+ * Parameters:
+ *
+ * Return:
+ *
+ * Author:
+ *
+ * History:
+ *      Date            Name                        Content
+ *
+***************************************************************/
+
+
+RC shutdownBufferPool(BM_BufferPool *const bm){
+
+}
+
+/***************************************************************
+ * Function Name: 
+ * 
+ * Description:
+ *
+ * Parameters:
+ *
+ * Return:
+ *
+ * Author:
+ *
+ * History:
+ *      Date            Name                        Content
+ *
+***************************************************************/
+
+RC forceFlushPool(BM_BufferPool *const bm){
+
+}
+
+// Buffer Manager Interface Access Pages
+
+/***************************************************************
+ * Function Name: 
+ * 
+ * Description:
+ *
+ * Parameters:
+ *
+ * Return:
+ *
+ * Author:
+ *
+ * History:
+ *      Date            Name                        Content
+ *
+***************************************************************/
+
+RC markDirty (BM_BufferPool *const bm, BM_PageHandle *const page){
+
+}
+
+/***************************************************************
+ * Function Name: 
+ * 
+ * Description:
+ *
+ * Parameters:
+ *
+ * Return:
+ *
+ * Author:
+ *
+ * History:
+ *      Date            Name                        Content
+ *
+***************************************************************/
+
+RC unpinPage (BM_BufferPool *const bm, BM_PageHandle *const page){
+
+}
+
+/***************************************************************
+ * Function Name: 
+ * 
+ * Description:
+ *
+ * Parameters:
+ *
+ * Return:
+ *
+ * Author:
+ *
+ * History:
+ *      Date            Name                        Content
+ *
+***************************************************************/
+
+RC forcePage (BM_BufferPool *const bm, BM_PageHandle *const page){
+
+}
+
+/***************************************************************
+ * Function Name: 
+ * 
+ * Description:
+ *
+ * Parameters:
+ *
+ * Return:
+ *
+ * Author:
+ *
+ * History:
+ *      Date            Name                        Content
+ *
+***************************************************************/
+
+RC pinPage (BM_BufferPool *const bm, BM_PageHandle *const page, 
+	    const PageNumber pageNum){
+
+}
+
+// Statistics Interface
+
+/***************************************************************
+ * Function Name: 
+ * 
+ * Description:
+ *
+ * Parameters:
+ *
+ * Return:
+ *
+ * Author:
+ *
+ * History:
+ *      Date            Name                        Content
+ *
+***************************************************************/
+
+PageNumber *getFrameContents (BM_BufferPool *const bm){
+
+}
+
+/***************************************************************
+ * Function Name: 
+ * 
+ * Description:
+ *
+ * Parameters:
+ *
+ * Return:
+ *
+ * Author:
+ *
+ * History:
+ *      Date            Name                        Content
+ *
+***************************************************************/
+
+bool *getDirtyFlags (BM_BufferPool *const bm){
+
+}
+
+/***************************************************************
+ * Function Name: 
+ * 
+ * Description:
+ *
+ * Parameters:
+ *
+ * Return:
+ *
+ * Author:
+ *
+ * History:
+ *      Date            Name                        Content
+ *
+***************************************************************/
+
+int *getFixCounts (BM_BufferPool *const bm){
+
+}
+
+/***************************************************************
+ * Function Name: 
+ * 
+ * Description:
+ *
+ * Parameters:
+ *
+ * Return:
+ *
+ * Author:
+ *
+ * History:
+ *      Date            Name                        Content
+ *
+***************************************************************/
+
+int getNumReadIO (BM_BufferPool *const bm){
+
+}
+
+/***************************************************************
+ * Function Name: 
+ * 
+ * Description:
+ *
+ * Parameters:
+ *
+ * Return:
+ *
+ * Author:
+ *
+ * History:
+ *      Date            Name                        Content
+ *
+***************************************************************/
+
+int getNumWriteIO (BM_BufferPool *const bm){
+
+}
